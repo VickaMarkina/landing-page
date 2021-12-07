71289468626f233e33de201e265731e97b1b4c2d
VickaMarkina <marckina.vika99@gmail.com>
2021-12-07 10:03:38 +0200

    header+slider+about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-(function($) {})(jQuery);
+(function($) {
+    
+let header = $(".header-navbar");
+let changeScroll = 100;
+
+// меню скрол
+$(window).on('scroll', () => {
+     $(window).scrollTop() > changeScroll
+     ? header.addClass('header-navbar--scrolled')
+     :header.removeClass('header-navbar--scrolled');
+}); 
+
+// отслеживание стр
+$(".nav").onePageNav({
+    currentClass: 'active',
+    scrollSpeed: 750,
+    easing: 'swing',
+    filter: ':not(.navbar-brand)'
+});
+
+// плагин анимации
+AOS.init({
+    disable: 'mobile',
+    duration: 600,
+    easing: 'ease-in-sine'
+});
+
+})(jQuery);
