6acd625eb2606cd6d5008c59ae089aa78209de84
VickaMarkina <marckina.vika99@gmail.com>
2021-12-09 20:40:04 +0200

    advantages + footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,34 @@
     
 let header = $(".header-navbar");
 let changeScroll = 100;
+let $btnBackTop = $('.backtop');
+
+// кнопка в начало стр
+const backTopOffset = 700;
+const backTopAnimationDuration = 500;
+const backTopAnimationFunction = 'swing';
+const backTop = (pos) =>
+  pos >= backTopOffset
+    ? $btnBackTop.fadeIn(500)
+    : $btnBackTop.fadeOut(500);
+
+$btnBackTop.on('click', () => {
+  $('html').animate(
+    {
+      scrollTop: 0
+    },
+    backTopAnimationDuration,
+    backTopAnimationFunction
+  );
+  return false;
+});
 
 // меню скрол
 $(window).on('scroll', () => {
      $(window).scrollTop() > changeScroll
      ? header.addClass('header-navbar--scrolled')
      :header.removeClass('header-navbar--scrolled');
+     backTop($(window).scrollTop());
 }); 
 
 // отслеживание стр
